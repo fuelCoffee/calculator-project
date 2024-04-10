b7348500e9ce2992ac79f62cc1578ddf75af6b80
fuelCoffee <fuelcoffee@users.noreply.github.com>
2024-04-10 01:43:53 -0500

    Create initial operate function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "Use Strict";
 
+let display = document.querySelector(".display");
+
 let num1;
 let num2;
 let operator;
@@ -19,4 +21,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return (result = num1 / num2);
+}
+
+function operate(num1, operator, num2) {
+  if (operator == "+") {
+    display.innerHTML = add(num1, num2);
+  } else if (operator == "-") {
+    display.innerHTML = subtract(num1, num2);
+  } else if (operator == "*") {
+    display.innerHTML = multiply(num1, num2);
+  } else if (operator == "/") {
+    display.innerHTML = divide(num1, num2);
+  }
 }
