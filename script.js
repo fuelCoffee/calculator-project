26c128cfc7118dd573ab5a4c863f8653a30ff581
fuelCoffee <fuelcoffee@users.noreply.github.com>
2024-04-10 01:05:02 -0500

    Create initial vars and math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+"Use Strict";
+
+let num1;
+let num2;
+let operator;
+let result;
+
+function add(num1, num2) {
+  return (result = num1 + num2);
+}
+
+function subtract(num1, num2) {
+  return (result = num1 - num2);
+}
+
+function multiply(num1, num2) {
+  return (result = num1 * num2);
+}
+
+function divide(num1, num2) {
+  return (result = num1 / num2);
+}
