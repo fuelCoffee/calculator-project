ee86a8c4038a09bfc9e365de934b34d56c844594
fuelCoffee <fuelcoffee@users.noreply.github.com>
2024-04-10 05:30:24 -0500

    Remove unnecessary part of math functions

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-  return (result = num1 - num2);
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return (result = num1 * num2);
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return (result = num1 / num2);
+  return num1 / num2;
 }
 
 function operate(number1, operator, number2) {
